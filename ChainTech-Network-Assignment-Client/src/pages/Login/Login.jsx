b0839bb540035e96b0c4a066a6eb885ae9d4c845
nhasan97 <nh6069325@gmail.com>
2024-02-03 19:36:31 +0600

    added css styling to registration and login forms

--- ChainTech-Network-Assignment-Client/src/pages/Login/Login.jsx
+++ ChainTech-Network-Assignment-Client/src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { BiLogoGoogle } from "react-icons/bi";
+import "./Login.css";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
@@ -11,67 +12,60 @@ const Login = () => {
   const onSubmit = (data) => {};
 
   return (
-    <div className="">
-      <div className="">
-        <div className="">
-          <form onSubmit={handleSubmit(onSubmit)} className="">
-            <h1 className="">Login</h1>
+    <div className="login-container">
+      <div className="login-form-container">
+        <form onSubmit={handleSubmit(onSubmit)} className="login-form">
+          <h1 className="">Login</h1>
 
-            <div className="">
-              <div
-                className="
-              "
-              >
-                <i className="fa-solid fa-envelope"></i>
-              </div>
-              <input
-                type="email"
-                id="in2"
-                {...register("email")}
-                placeholder="Email"
-                required
-                className=""
-              />
+          <div className="input-holder">
+            <div className="input-icon-holder">
+              <i className="fa-solid fa-envelope"></i>
             </div>
-
-            <div className="">
-              <div
-                className="
-              "
-              >
-                <i className="fa-solid fa-key"></i>
-              </div>
-
-              <input
-                type={showPass ? "text" : "password"}
-                id="in3"
-                {...register("pass")}
-                placeholder="Password"
-                required
-                className=""
-              />
-              <span className="" onClick={() => setShowPass(!showPass)}>
-                {showPass ? (
-                  <i className="fa-solid fa-eye-slash"></i>
-                ) : (
-                  <i className="fa-solid fa-eye"></i>
-                )}
-              </span>
-            </div>
-
-            <input type="submit" value="Sign In" className="" />
-          </form>
-
-          <div className="">
-            <p className="">
-              New here?
-              <Link className="" to="/register">
-                Create a New Account
-              </Link>
-            </p>
-            <p className="">Or sign in with</p>
-            <BiLogoGoogle className=""></BiLogoGoogle>
+            <input
+              type="email"
+              id="in2"
+              {...register("email")}
+              placeholder="Email"
+              required
+              className=""
+            />
           </div>
+
+          <div className="input-holder">
+            <div className="input-icon-holder">
+              <i className="fa-solid fa-key"></i>
+            </div>
+
+            <input
+              type={showPass ? "text" : "password"}
+              id="in3"
+              {...register("pass")}
+              placeholder="Password"
+              required
+              className=""
+            />
+            <span className="eye-icon" onClick={() => setShowPass(!showPass)}>
+              {showPass ? (
+                <i className="fa-solid fa-eye-slash"></i>
+              ) : (
+                <i className="fa-solid fa-eye"></i>
+              )}
+            </span>
+          </div>
+
+          <input type="submit" value="Sign In" className="btn" />
+        </form>
+        <div className="other-option-container">
+          <p className="">
+            New here?
+            <Link className="" to="/register">
+              Create a New Account
+            </Link>
+          </p>
+          <p className="">Or sign in with</p>
+          <button className="btn google-btn">
+            <BiLogoGoogle className="google-icon"></BiLogoGoogle>
+          </button>
         </div>
       </div>
     </div>
--- ChainTech-Network-Assignment-Client/src/pages/Register/Register.jsx
+++ ChainTech-Network-Assignment-Client/src/pages/Register/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { BiLogoGoogle } from "react-icons/bi";
+import "./Register.css";
 
 const Register = () => {
   const {
@@ -18,179 +19,180 @@ const Register = () => {
   const onSubmit = async (data) => {};
 
   return (
-    <div className="">
-      <div className="">
-        <div className="">
-          <form className="" onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="">Sign UP</h1>
+    <div className="reg-container">
+      <div className="reg-form-container">
+        <form className="reg-form" onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="">Sign UP</h1>
 
-            <div>
-              <div>
-                <div className="">
-                  <div className="">
-                    <i className="fa-solid fa-signature"></i>
-                  </div>
-                  <input
-                    id="in1"
-                    type="text"
-                    {...register("name", { required: true })}
-                    placeholder="Name"
-                    className=""
-                  />
-                  {errors.name?.type === "required" && (
-                    <p className="text-red-500">Name is required</p>
-                  )}
+          <div className="input-container">
+            <div className="four-input-divider">
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-solid fa-signature"></i>
                 </div>
-
-                <div className="">
-                  <div className="">
-                    <i className="fa-solid fa-envelope"></i>
-                  </div>
-                  <input
-                    id="in2"
-                    type="email"
-                    {...register("email", { required: true })}
-                    placeholder="Email"
-                    className=""
-                  />
-                  {errors.email?.type === "required" && (
-                    <p className="">Email is required</p>
-                  )}
-                </div>
-
-                <div className="">
-                  <div className="">
-                    <i className="fa-solid fa-key"></i>
-                  </div>
-                  <input
-                    id="in3"
-                    type={showPass ? "text" : "password"}
-                    {...register("pass", {
-                      required: true,
-                      minLength: 6,
-                      maxLength: 20,
-                      pattern:
-                        /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
-                    })}
-                    placeholder="Password"
-                    className=""
-                  />
-                  <span className="" onClick={() => setShowPass(!showPass)}>
-                    {showPass ? (
-                      <i className="fa-solid fa-eye-slash"></i>
-                    ) : (
-                      <i className="fa-solid fa-eye"></i>
-                    )}
-                  </span>
-                  {errors.pass?.type === "required" && (
-                    <p className="">Password is required</p>
-                  )}
-                  {errors.pass?.type === "minLength" && (
-                    <p className="">
-                      Password has to be at least 6 characters long
-                    </p>
-                  )}
-                  {errors.pass?.type === "pattern" && (
-                    <p className="">
-                      <ul className="list-disc">
-                        Password must have at least
-                        <li>1 uppercase letter</li>
-                        <li>1 lowercase letter</li>
-                        <li>1 digit</li>
-                        <li>1 special character</li>
-                      </ul>
-                    </p>
-                  )}
-                </div>
-
-                <div className="">
-                  <label className="">
-                    <span className="">Pick your profile picture</span>
-                  </label>
-                  <input type="file" {...register("photo")} className="" />
-                </div>
+                <input
+                  id="in1"
+                  type="text"
+                  {...register("name", { required: true })}
+                  placeholder="Name"
+                  className=""
+                />
+                {errors.name?.type === "required" && (
+                  <p className="text-red-500">Name is required</p>
+                )}
               </div>
 
-              <div>
-                <div className="">
-                  <div className="">
-                    <i className="fa-solid fa-signature"></i>
-                  </div>
-                  <input
-                    id="in1"
-                    type="text"
-                    {...register("cell", { required: true })}
-                    placeholder="Phone Number"
-                    className=""
-                  />
-                  {errors.name?.type === "required" && (
-                    <p className="text-red-500">Name is required</p>
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-solid fa-envelope"></i>
+                </div>
+                <input
+                  id="in2"
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="Email"
+                  className=""
+                />
+                {errors.email?.type === "required" && (
+                  <p className="">Email is required</p>
+                )}
+              </div>
+
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-solid fa-key"></i>
+                </div>
+                <input
+                  id="in3"
+                  type={showPass ? "text" : "password"}
+                  {...register("pass", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
+                  placeholder="Password"
+                  className=""
+                />
+                <span
+                  className="eye-icon"
+                  onClick={() => setShowPass(!showPass)}
+                >
+                  {showPass ? (
+                    <i className="fa-solid fa-eye-slash"></i>
+                  ) : (
+                    <i className="fa-solid fa-eye"></i>
                   )}
-                </div>
+                </span>
+                {errors.pass?.type === "required" && (
+                  <p className="">Password is required</p>
+                )}
+                {errors.pass?.type === "minLength" && (
+                  <p className="">
+                    Password has to be at least 6 characters long
+                  </p>
+                )}
+                {errors.pass?.type === "pattern" && (
+                  <p className="">
+                    <ul className="list-disc">
+                      Password must have at least
+                      <li>1 uppercase letter</li>
+                      <li>1 lowercase letter</li>
+                      <li>1 digit</li>
+                      <li>1 special character</li>
+                    </ul>
+                  </p>
+                )}
+              </div>
 
-                <div className="relative">
-                  <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
-                    <i className="fa-solid fa-d text-xl text-white"></i>
-                  </div>
-                  <select
-                    type="text"
-                    {...register("gender")}
-                    placeholder="Gender"
-                    required
-                    className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
-                  >
-                    {genders.map((gender) => (
-                      <option key={gender}>{gender}</option>
-                    ))}
-                  </select>
-                </div>
-
-                <div className="relative">
-                  <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
-                    <i className="fa-regular fa-calendar-days text-xl text-white"></i>
-                  </div>
-                  <input
-                    type="date"
-                    id="in4"
-                    {...register("birthday")}
-                    placeholder="Birthday"
-                    required
-                    className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
-                  />
-                </div>
-
-                <div className="">
-                  <div className="">
-                    <i className="fa-solid fa-signature"></i>
-                  </div>
-                  <input
-                    id="in1"
-                    type="text"
-                    {...register("address", { required: true })}
-                    placeholder="Address"
-                    className=""
-                  />
-                  {errors.name?.type === "required" && (
-                    <p className="text-red-500">Name is required</p>
-                  )}
-                </div>
+              <div className="input-holder">
+                <label className="input-icon-holder">
+                  <i className="fa-solid fa-image"></i>
+                </label>
+                <input type="file" {...register("photo")} className="" />
               </div>
             </div>
 
-            <input type="submit" value="Sign Up" className="" />
-          </form>
+            <div className="four-input-divider">
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-solid fa-signature"></i>
+                </div>
+                <input
+                  id="in1"
+                  type="text"
+                  {...register("cell", { required: true })}
+                  placeholder="Phone Number"
+                  className=""
+                />
+                {errors.name?.type === "required" && (
+                  <p className="text-red-500">Name is required</p>
+                )}
+              </div>
 
-          <div className="">
-            <p className="">
-              Already registered?
-              <Link className="" to="/login">
-                Go to log in
-              </Link>
-            </p>
-            <p className="">Or sign in with</p>
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-solid fa-d"></i>
+                </div>
+                <select
+                  type="text"
+                  {...register("gender")}
+                  placeholder="Gender"
+                  required
+                  className=""
+                >
+                  {genders.map((gender) => (
+                    <option key={gender}>{gender}</option>
+                  ))}
+                </select>
+              </div>
 
-            <BiLogoGoogle className=""></BiLogoGoogle>
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-regular fa-calendar-days"></i>
+                </div>
+                <input
+                  type="date"
+                  id="in4"
+                  {...register("birthday")}
+                  placeholder="Birthday"
+                  required
+                  className=""
+                />
+              </div>
+
+              <div className="input-holder">
+                <div className="input-icon-holder">
+                  <i className="fa-solid fa-signature"></i>
+                </div>
+                <input
+                  id="in1"
+                  type="text"
+                  {...register("address", { required: true })}
+                  placeholder="Address"
+                  className=""
+                />
+                {errors.name?.type === "required" && (
+                  <p className="text-red-500">Name is required</p>
+                )}
+              </div>
+            </div>
           </div>
+
+          <input type="submit" value="Sign Up" className="btn reg-btn" />
+        </form>
+
+        <div className="other-option-container">
+          <p className="">
+            Already registered?
+            <Link className="" to="/login">
+              Go to log in
+            </Link>
+          </p>
+          <p className="">Or sign in with</p>
+          <button className="btn google-btn">
+            <BiLogoGoogle className="google-icon"></BiLogoGoogle>
+          </button>
         </div>
       </div>
     </div>
